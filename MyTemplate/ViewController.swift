2f5cace28512534fcf8c0ec4580b1073a70270b3
YinYin Chiu <cychiuae@connect.ust.hk>
2018-05-28 00:06:17 +0800

    Initial commit

---
+++ MyTemplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTemplate
+//
+//  Created by YinYin Chiu on 28/5/2018.
+//  Copyright © 2018 YinYin Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
