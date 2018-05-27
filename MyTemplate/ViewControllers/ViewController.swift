5f7dc1a6b8db1aca025301f1449be35762f05722
YinYin Chiu <cychiuae@connect.ust.hk>
2018-05-28 01:52:27 +0800

    Bind views and actions

--- MyTemplate/ViewControllers/ViewController.swift
+++ MyTemplate/ViewControllers/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import RxSwiftExt
 import SnapKit
 
 fileprivate enum styles {
@@ -22,6 +25,11 @@ fileprivate enum styles {
     }
 }
 
+protocol ViewControllerActionDelegate: class {
+    func didPressIncrementButton()
+    func didPressDecrementButton()
+}
+
 struct ViewControllerState: Equatable {
     let count: Int
 }
@@ -31,20 +39,42 @@ class ViewControllerReactor: StatefulReactor {
     typealias Props = Void
     let defaultProps: Void = ()
     var initialState = ViewControllerState(count: 0)
+
+    let app: App
+
+    init(app: App) {
+        self.app = app
+    }
+}
+
+extension ViewControllerReactor: ViewControllerActionDelegate {
+    func didPressIncrementButton() {
+        CounterActionCreator.increment(by: 1, app: self.app)
+    }
+
+    func didPressDecrementButton() {
+        CounterActionCreator.decrement(by: 1, app: self.app)
+    }
 }
 
 class ViewController: UIViewController, StatefulComponent {
     typealias Reactor = ViewControllerReactor
-    let reactor = Reactor()
+    lazy var reactor: Reactor = {
+        let reactor = Reactor(app: self.app)
+        self.actionDelegate = reactor
+        return reactor
+    }()
+    private weak var actionDelegate: ViewControllerActionDelegate?
+    private var disposeBag = DisposeBag()
 
-    let counterView = CounterView()
-    let incrementButton: UIButton = {
+    private let counterView = CounterView()
+    private let incrementButton: UIButton = {
         let button = UIButton()
         button.apply(styles.incrementButton)
         button.setTitle("Increment", for: .normal)
         return button
     }()
-    let decrementButton: UIButton = {
+    private let decrementButton: UIButton = {
         let button = UIButton()
         button.apply(styles.decrementButton)
         button.setTitle("Decrement", for: .normal)
@@ -76,5 +106,41 @@ class ViewController: UIViewController, StatefulComponent {
             make.width.equalTo(self.incrementButton)
             make.right.bottom.equalToSuperview()
         }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        let scheduler = ConcurrentDispatchQueueScheduler(qos: .userInitiated)
+        let counterStateObservable = self.app.store
+            .createObservable { $0.counterState }
+            .unwrap()
+        Observable.combineLatest(self.reactor.state, counterStateObservable, resultSelector: {$0.count + $1.count})
+            .observeOn(scheduler)
+            .unwrap()
+            .map { CounterViewProps(count: $0) }
+            .bind(to: self.counterView.reactor.props)
+            .disposed(by: self.disposeBag)
+
+        self.incrementButton.rx.tap
+            .asObservable()
+            .do(onNext: {
+                self.actionDelegate?.didPressIncrementButton()
+            })
+            .subscribe()
+            .disposed(by: self.disposeBag)
+
+        self.decrementButton.rx.tap
+            .asObservable()
+            .do(onNext: {
+                self.actionDelegate?.didPressIncrementButton()
+            })
+            .subscribe()
+            .disposed(by: self.disposeBag)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.disposeBag = DisposeBag()
     }
 }
