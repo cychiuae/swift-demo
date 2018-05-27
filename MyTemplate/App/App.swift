4b61470a5245fc11e1fda6046c194e61d74de7a4
YinYin Chiu <cychiuae@connect.ust.hk>
2018-05-28 00:24:24 +0800

    Setup App

---
+++ MyTemplate/App/App.swift
@@ -0,0 +1,21 @@
+//
+//  App.swift
+//  MyTemplate
+//
+//  Created by YinYin Chiu on 28/5/2018.
+//  Copyright © 2018 YinYin Chiu. All rights reserved.
+//
+
+import Foundation
+
+protocol App {
+    var store: ReduxStore { get }
+}
+
+class TemplateApp: App {
+    let store: ReduxStore
+
+    init(store: ReduxStore) {
+        self.store = store
+    }
+}
