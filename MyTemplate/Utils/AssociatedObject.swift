51952bc34b6d77a9082fbdc10d8fbed313e0fa18
YinYin Chiu <cychiuae@connect.ust.hk>
2018-05-28 00:32:42 +0800

    Add associated object

---
+++ MyTemplate/Utils/AssociatedObject.swift
@@ -0,0 +1,30 @@
+//
+//  AssociatedObject.swift
+//  MyTemplate
+//
+//  Created by YinYin Chiu on 28/5/2018.
+//  Copyright © 2018 YinYin Chiu. All rights reserved.
+//
+import ObjectiveC
+
+protocol AssociatedObject {
+}
+
+extension AssociatedObject {
+    func associatedObject<T>(forKey key: UnsafeRawPointer) -> T? {
+        return objc_getAssociatedObject(self, key) as? T
+    }
+
+    func associatedObject<T>(forKey key: UnsafeRawPointer, initialValue: T) -> T {
+        if let object: T = self.associatedObject(forKey: key) {
+            return object
+        }
+        let object = initialValue
+        self.setAssociatedObject(object, forKey: key)
+        return object
+    }
+
+    func setAssociatedObject<T>(_ object: T?, forKey key: UnsafeRawPointer) {
+        objc_setAssociatedObject(self, key, object, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+}
