11aabd7c2551b7ca956585c16ceab44bb89c1dd3
YinYin Chiu <cychiuae@connect.ust.hk>
2018-05-28 01:03:24 +0800

    Add Style Helper

---
+++ MyTemplate/Utils/Style.swift
@@ -0,0 +1,31 @@
+//
+//  Style.swift
+//  MyTemplate
+//
+//  Created by YinYin Chiu on 28/5/2018.
+//  Copyright © 2018 YinYin Chiu. All rights reserved.
+//
+
+import UIKit
+
+struct Style<View: UIView> {
+    let style: (View) -> Void
+
+    public func apply(to view: View) {
+        style(view)
+    }
+
+    public func compose(_ styles: Style<View>...) -> Style<View> {
+        return Style { view in 
+            for style in styles {
+                style.apply(to: view)
+            }
+        }
+    }
+}
+
+extension UIView {
+    func apply<View: UIView>(_ style: Style<View>) {
+        style.apply(to: (self as! View))
+    }
+}
